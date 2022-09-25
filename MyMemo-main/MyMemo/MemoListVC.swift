db6b68227543e875298ee4928d89249b63258cad
DDangle <101912067+DDangle@users.noreply.github.com>
2022-09-25 18:40:26 +0900

    09 24 Update

--- MyMemo-main/MyMemo/MemoListVC.swift
+++ MyMemo-main/MyMemo/MemoListVC.swift
@@ -14,7 +14,13 @@ class MemoListVC : UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("메모 데이터 \([MemoData]())")
+        
+    }
+    
+    //디바이스 스크린에 뷰컨트롤러가 나타날 때마다 호출되는 메소드
+    override func viewWillAppear(_ animated: Bool) {
+        //테이블 데이터를 다시 읽어들인다. 이에따라 행을 구성하는 로직이 다시 실행
+        self.tableView.reloadData()
     }
     
     
@@ -31,37 +37,36 @@ class MemoListVC : UITableViewController {
         let count = self.appDelegate.memolist.count
         return count
         
-        print("numberOfRowsInsection 앱 델리게이트 메모리스트 \(self.appDelegate.memolist)")
+        
     }
     
     //테이블 행을 구성하는 메소드 - 셀
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //memolist 배열 데이터에서 주어진 행에 맞는 데이터를 꺼낸다.
         let row = self.appDelegate.memolist[indexPath.row]
-        let celled = "MemoCell"
-        let cell = tableView.dequeueReusableCell(withIdentifier: celled) as! MemoCell
+        let cellId = row.title == nil ? "memoCell" : "memoCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId) as! MemoCell
+        
         
         //memoCell 의 값
         cell.mymemoTitle?.text = row.title
         cell.mymemoContents?.text = row.contents
         
-//        //Date 타입의 날짜를 yyyy-mm-dd HH:mm:ss  포맷에 맞게 변경한다.
-//        let formatter = DateFormatter()
-//        formatter.dateFormat = "yyyy-mm-dd HH:mm:ss"
-//        cell.regDate?.text = formatter.string(from: row.regdata!)
-//        
         return cell
     }
     //테이블에 특정행이 선택되었을때 실행되는 메소드, 선택된 행의 정보는 indexPath 에 담겨 전달
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
         //memoList 배열에서 선택된 행에 맞는 데이터를 꺼냄
         let row = self.appDelegate.memolist[indexPath.row]
+        print("3333333333")
         
         //MemoReadVC 화면의 인스턴스 생성
         guard let vc = self.storyboard?.instantiateViewController(withIdentifier: "MemoRead") as? MemoReadVC else { //instantiateViewController 지정된 식별자로 뷰컨트롤로를만들고 스토리보드 데이터로 초기화 한다.
             return
         }
         
+        print("11111111")
         //값 전달후 MemoReadVC로 이동한다
         vc.param = row
         self.navigationController?.pushViewController(vc, animated: true)
--- MyMemo-main/MyMemo/MemoWriteVC.swift
+++ MyMemo-main/MyMemo/MemoWriteVC.swift
@@ -32,7 +32,7 @@ class MemoWriteVC : UIViewController, UINavigationControllerDelegate, UITextView
         let data = MemoData()
 
         //제목
-        data.title = self.subject
+//        data.title = self.subject
         //내용
         data.contents = self.detailTextView.text
 
@@ -43,8 +43,7 @@ class MemoWriteVC : UIViewController, UINavigationControllerDelegate, UITextView
         //글작성 화면을 종료하고 memoList로 돌아간다
         //popViewController - 탐색 스택에서 상위뷰컨트롤러를 팝하고 디스플레이를 업데이트
         _ = self.navigationController?.popViewController(animated: true)
-        print("세이브 버튼 왜 안눌림?")
-        print("메모데이터 왜 못불러오냐 \([MemoData]())")
+       
     }
     
     //사용자가 텍스트뷰에 뭔가 입력하면 호출되는 메소드
